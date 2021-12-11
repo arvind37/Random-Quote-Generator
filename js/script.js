634a10a4fad104b4cb5c36a17325a5ccdb64189a
Arvind Kumawat <55478740+arvind37@users.noreply.github.com>
2021-12-11 18:20:19 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function quoteGenerator() {
+
+    var quotes = [
+        {quote: " The greatest glory in living lies not in never falling, but in rising every time we fall.",
+         author: "- Nelson Mandela"},
+         {quote: " The way to get started is to quit talking and begin doing.",
+         author: "- Walt Disney"},
+         {quote: " Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+         author: "- Steve Jobs"},
+         {quote: " If life were predictable it would cease to be life, and be without flavor.",
+         author: "- Eleanor Roosevelt"},
+         {quote: " If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+         author: "- Oprah Winfrey"},
+         {quote: " If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+         author: "- James Cameron"},
+    ];
+
+
+    let random = Math.floor(Math.random() * quotes.length);
+    console.log(random);
+
+    document.getElementById('quote').innerHTML = quotes[random].quote;
+    document.getElementById('author').innerHTML = quotes[random].author;
+ 
+
+
+}
